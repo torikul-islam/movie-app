bb6ef915c2d57f072a1f8d8fca49a0b01cb724a6
torikul-islam <torikulislam024@gmail.com>
2019-03-09 19:11:54 +0600

    add font awesome icon for sorting

--- src/components/common/TableHeader.js
+++ src/components/common/TableHeader.js
@@ -14,17 +14,26 @@ class TableHeader extends Component {
         this.props.onSort(sortColumn);
     };
 
+    renderSortIcon=(column)=>{
+        const { sortColumn } =this.props;
+        if(column.path !== sortColumn.path) return null;
+        if(sortColumn.order ==="asc")return <i className="fa fa-sort-asc"/>
+
+        return <i className="fa fa-sort-desc"></i>
+
+    };
+
     render() {
         return (
             <thead>
-            <tr>
+            <tr style={{cursor: "pointer"}}>
                 {
                     this.props.columns.map(column =>
                         <th
                             key={column.path || column.key}
                             onClick={() => this.raiseSort(column.path)}
                         >
-                         {column.label}
+                         {column.label} {this.renderSortIcon(column)}
                         </th>
                     )}
             </tr>
