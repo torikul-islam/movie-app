26943dffc0a6da06fda63211b9a8740ce9d28304
torikul-islam <torikulislam024@gmail.com>
2019-03-10 00:12:58 +0600

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
-import './App.css';
+import {Route, Redirect, Switch} from "react-router-dom";
 import Movies from "./components/common/Movies";
+import Customers from "./components/Customers";
+import Rentals from "./components/Rentals";
+import NotFound from "./components/NotFound"
+import Navbar from "./components/common/Navbar";
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <React.Fragment>
-          <div className="container">
-              <Movies />
-          </div>
-        </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar/>
+                <main className="container">
+                    <Switch>
+                        <Route path="/movies/" component={Movies} exact/>
+                        <Route path="/customers/" component={Customers} exact/>
+                        <Route path="/rentals/" component={Rentals} exact/>
+                        <Route path="/not-found/" component={NotFound} exact/>
+                        <Redirect from="/" to="/movies/" exact/>
+                        <Redirect to="/not-found/"/>
+                    </Switch>
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- src/components/MoviesTable.js
+++ src/components/MoviesTable.js
@@ -1,35 +1,35 @@
 import React, {Component} from "react";
 import Table from "./common/Table";
 
-
 class MoviesTable extends Component {
     columns = [
         {path: "title", label: "Title"},
         {path: "genre.name", label: "Genre"},
         {path: "numberInStock", label: "Stock"},
         {path: "dailyRentalRate", label: "Rate"},
-        {key: "delete",
-         content: movie=> <button
-                onClick={() => this.props.onDelete(movie)}
-                className="btn btn-danger btn-sm"
-            >
-                Delete
-            </button> },
-
+        {
+            key: "delete",
+            content: movie => (
+                <button
+                    onClick={() => this.props.onDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                >
+                    Delete
+                </button>
+            )
+        }
     ];
-
 
     render() {
         const {movies, sortColumn, onSort} = this.props;
         return (
             <Table
-             columns={this.columns}
-             data={movies}
-             sortColumn={sortColumn}
-             onSort={onSort}
+                columns={this.columns}
+                data={movies}
+                sortColumn={sortColumn}
+                onSort={onSort}
             />
         );
-
     }
 }
 
